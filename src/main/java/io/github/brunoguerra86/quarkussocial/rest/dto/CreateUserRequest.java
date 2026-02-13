e1a8f28643765600af9cddc2ee010f75e7c713fb
brunoguerra <brunoguerra@hotmail.com>
2026-02-13 16:22:48 -0300

    feat: lombok

--- src/main/java/io/github/brunoguerra86/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/brunoguerra86/quarkussocial/rest/dto/CreateUserRequest.java
@@ -2,7 +2,10 @@ package io.github.brunoguerra86.quarkussocial.rest.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+
+@Data
 public class CreateUserRequest {
 
     @NotBlank(message = "Name is required")
@@ -10,19 +13,4 @@ public class CreateUserRequest {
     @NotNull(message = "Age is required")
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
