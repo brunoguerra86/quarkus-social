ace3d309298faac2b7ec2401b76b541e62e56122
brunoguerra <brunoguerra@hotmail.com>
2026-02-24 11:08:16 -0300

    feat: PUT follower

--- src/main/java/io/github/brunoguerra86/quarkussocial/rest/FollowerResource.java
+++ src/main/java/io/github/brunoguerra86/quarkussocial/rest/FollowerResource.java
@@ -1,12 +1,14 @@
 package io.github.brunoguerra86.quarkussocial.rest;
 
+import io.github.brunoguerra86.quarkussocial.domain.model.Follower;
 import io.github.brunoguerra86.quarkussocial.domain.repository.FollowerRepository;
 import io.github.brunoguerra86.quarkussocial.domain.repository.UserRepository;
+import io.github.brunoguerra86.quarkussocial.rest.dto.FollowerRequest;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -20,5 +22,28 @@ public class FollowerResource {
     public FollowerResource(FollowerRepository followerRepository, UserRepository userRepository) {
         this.followerRepository = followerRepository;
         this.userRepository = userRepository;
+    }
+
+    @PUT
+    @Transactional
+    public Response followUser(@PathParam("userId") Long userId, FollowerRequest request){
+        var userToFollow = userRepository.findById(userId);
+        if(userToFollow == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        var follower = userRepository.findById(request.getFollowerId());
+
+        boolean follows = followerRepository.follows(follower, userToFollow);
+
+        if (!follows) {
+            var entity = new Follower();
+            entity.setUser(userToFollow);
+            entity.setFollower(follower);
+
+            followerRepository.persist(entity);
+        }
+
+        return Response.status(Response.Status.NO_CONTENT).build();
     }
 }
---
+++ src/main/java/io/github/brunoguerra86/quarkussocial/rest/dto/FollowerRequest.java
@@ -0,0 +1,8 @@
+package io.github.brunoguerra86.quarkussocial.rest.dto;
+
+import lombok.Data;
+
+@Data
+public class FollowerRequest {
+    private Long followerId;
+}
