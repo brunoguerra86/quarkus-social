2df291d4f3fbab8e4dd4b9e41aade73aef74fdbb
brunoguerra <brunoguerra@hotmail.com>
2026-02-24 11:11:48 -0300

    feat: PUT follower - conflict validation

--- src/main/java/io/github/brunoguerra86/quarkussocial/rest/FollowerResource.java
+++ src/main/java/io/github/brunoguerra86/quarkussocial/rest/FollowerResource.java
@@ -27,6 +27,11 @@ public class FollowerResource {
     @PUT
     @Transactional
     public Response followUser(@PathParam("userId") Long userId, FollowerRequest request){
+
+        if(userId.equals(request.getFollowerId())){
+            return Response.status(Response.Status.CONFLICT).entity("You can't follow yourself").build();
+        }
+
         var userToFollow = userRepository.findById(userId);
         if(userToFollow == null){
             return Response.status(Response.Status.NOT_FOUND).build();
